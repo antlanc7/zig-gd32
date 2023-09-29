2ad2976c09781105707f3c8e89953ca25379f2d2
Antonio Lanciotti <a.lanciotti@westernengineering.it>
2023-09-29 14:23:34 +0200

    uart7 working

--- src/main.zig
+++ src/main.zig
@@ -5,59 +5,57 @@ const systick = @import("systick.zig");
 const std = @import("std");
 const IRC_FREQ = 16000000;
 
-// fn uart_write(uart: *volatile gd32_types.USART, bytes: []const u8) error{}!usize {
-//     for (bytes) |byte| {
-//         uart.TDR.write_raw(byte);
-//         while (uart.ISR.read().TC != 1) asm volatile ("");
-//     }
-//     return bytes.len;
-// }
+fn uart_write(uart: *volatile gd32_types.UART3, bytes: []const u8) error{}!usize {
+    for (bytes) |byte| {
+        uart.DATA.write_raw(byte);
+        while (uart.STAT0.read().TBE != 1) asm volatile ("");
+    }
+    return bytes.len;
+}
 
-// const uart2_writer = std.io.Writer(
-//     *volatile gd32_types.USART,
-//     error{},
-//     uart_write,
-// ){
-//     .context = gd32.USART2,
-// };
+const uart7_writer = std.io.Writer(
+    *volatile gd32_types.UART3,
+    error{},
+    uart_write,
+){
+    .context = gd32.UART7,
+};
 
 pub fn main() noreturn {
     systick.init(IRC_FREQ / 1000);
 
     // enable clock for gpio A and C
     gd32.RCU.AHB1EN.modify(.{
-        .PAEN = 1,
         .PCEN = 1,
+        .PEEN = 1,
     });
-    // // enable usart 2 (attached to usb vcom via gpio A2 and A15)
-    // gd32.RCU.APB1EN.modify(.{ .USART2EN = 1 });
-    // // pin 2 and 15 mode alternate function
-    // gd32.GPIOA.CTL.modify(.{
-    //     .CTL2 = 2,
-    //     .CTL15 = 2,
-    // });
-    // // pin 2 and 15 alternate function 1 = uart
-    // gd32.GPIOA.AFRL.modify(.{ .AFRL2 = 1 });
-    // gd32.GPIOA.AFRH.modify(.{ .AFRH15 = 1 });
+    // enable usart 2 (attached to usb vcom via gpio A2 and A15)
+    gd32.RCU.APB1EN.modify(.{ .UART7EN = 1 });
+    // pin 2 and 15 mode alternate function
+    gd32.GPIOE.CTL.modify(.{
+        .CTL0 = 2,
+        .CTL1 = 2,
+    });
+    // pin 0 and 1 alternate function 8 = uart
+    gd32.GPIOE.AFSEL0.modify(.{ .SEL0 = 8, .SEL1 = 8 });
 
-    // // usart 2 configuration: 115200 baudrate, only transmit and enable
-    // gd32.USART2.BRR.write_raw(IRC_FREQ / 115200);
-    // gd32.USART2.CR1.modify(.{
-    //     // .RE = 1,
-    //     .TE = 1,
-    //     .UE = 1,
-    // });
+    // usart 2 configuration: 115200 baudrate, only transmit and enable
+    gd32.UART7.BAUD.write_raw(IRC_FREQ / 115200);
+    gd32.UART7.CTL0.modify(.{
+        // .RE = 1,
+        .TEN = 1,
+        .UEN = 1,
+    });
 
     // gpio b3 output (onboard led)
     gd32.GPIOC.CTL.modify(.{ .CTL4 = 1 });
-    var counter: u32 = 0;
     const delay_ms = 500;
     while (true) {
-        // uart2_writer.print("ciao da zig, {}\r\n", .{counter}) catch unreachable;
+        const curr_sec = systick.getTicks() / 1000;
+        uart7_writer.print("ciao da zig, {}\r\n", .{curr_sec}) catch unreachable;
         gd32.GPIOC.BOP.modify(.{ .BOP4 = 1 }); // set gpio out value
         systick.delay(delay_ms);
         gd32.GPIOC.BC.modify(.{ .CR4 = 1 }); // set gpio out value
         systick.delay(delay_ms);
-        counter +%= 1;
     }
 }
